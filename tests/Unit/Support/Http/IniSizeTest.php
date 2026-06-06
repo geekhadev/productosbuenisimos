45160ecf9dc541cabc351e609b309d783e8a87b4
Irwing Naranjo <khalisser@gmail.com>
2026-06-05 23:03:25 -0400

    Remove deprecated feature tests for HomeLanding, PublicLandingProductShow, SiiAdminModulesRoutes, SalesAgent, CreateCustomerAddress, CreateCustomer, CreateOrder, GetCustomerByPhone, GetProducts, and related tools. These tests are no longer relevant and have been deleted to streamline the test suite.

--- tests/Unit/Support/Http/IniSizeTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Support\Http\IniSize;
-
-test('ini size parser converts megabytes to bytes', function () {
-    expect(IniSize::toBytes('2M'))->toBe(2 * 1024 * 1024)
-        ->and(IniSize::toBytes('256M'))->toBe(256 * 1024 * 1024)
-        ->and(IniSize::human('2M'))->toBe('2 MB');
-});
